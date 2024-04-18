ac1bc52456bb8d9cb22371a2adf59b06fdd35ed6
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-04-18 11:28:57 +0200

    import helper from taskfunction

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // TASK: import helper functions from utils
+import {getTasks, saveTasks, createNewTask, patchTask, putTask, deleteTask  } from './utils/taskFunctions.js'
 // TASK: import initialData
 
 
